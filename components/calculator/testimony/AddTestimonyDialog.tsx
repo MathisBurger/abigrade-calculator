33186c69fb14e630973f7e1a32ccc94dae314838
Mathis Burger <mburger@die-netzwerkstatt.de>
2023-01-23 18:25:00 +0100

    [TASK] Added testimony add feature

---
+++ components/calculator/testimony/AddTestimonyDialog.tsx
@@ -0,0 +1,70 @@
+import React, {useMemo, useState} from "react";
+import {Semester, Testimony} from "./TestimonyTopLayer";
+import {Button, Dialog, DialogActions, DialogContent, DialogTitle, MenuItem, Select, TextField} from "@mui/material";
+
+
+interface AddTestimonyDialogProps {
+    testimonies: Testimony[];
+    setTestimonies: (testomonies: Testimony[]) => void;
+    open: boolean;
+    onClose: () => void;
+}
+
+const AddTestimonyDialog: React.FC<AddTestimonyDialogProps> = ({testimonies, setTestimonies, open, onClose}) => {
+
+    const [name, setName] = useState<string>('');
+    const [semester, setSemester] = useState<Semester|undefined>();
+
+    const leftOptions = useMemo<Semester[]>(
+        () => {
+            let options = [Semester.Q1_1, Semester.Q1_2, Semester.Q2_1, Semester.Q2_2];
+            for (let testimony of testimonies) {
+                if (options.indexOf(testimony.semester) > -1) {
+                    options = options.filter((s) => s != testimony.semester);
+                }
+            }
+            return options;
+        },
+        [testimonies]
+    );
+
+    const createTestimony = () => {
+        if (semester !== undefined && name !== '') {
+            setTestimonies([
+                ...testimonies,
+                {
+                    name, semester, grades: []
+                }
+            ]);
+            onClose();
+        }
+    }
+
+    return (
+        <Dialog open={open} onClose={onClose}>
+            <DialogTitle>Add Testimony</DialogTitle>
+            <DialogContent>
+                <Select
+                    value={`${semester ?? ''}`}
+                    onChange={(e) => setSemester(e.target.value as Semester)}
+                    fullWidth
+                >
+                    {leftOptions.map((option) => (
+                        <MenuItem value={option} key={option}>{option}</MenuItem>
+                    ))}
+                </Select>
+                <TextField
+                    value={name}
+                    sx={{marginTop: '20px'}}
+                    label="Name"
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button variant="contained" color="primary" onClick={createTestimony}>Create</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default AddTestimonyDialog;
--- components/calculator/testimony/TestimonySideList.tsx
+++ components/calculator/testimony/TestimonySideList.tsx
@@ -1,6 +1,8 @@
-import {Grid, ListItemText, MenuItem, MenuList, Typography} from "@mui/material";
-import React from "react";
+import {Card, CardContent, Grid, ListItemIcon, ListItemText, MenuItem, MenuList, Typography} from "@mui/material";
+import React, {useState} from "react";
 import {Testimony} from "./TestimonyTopLayer";
+import {Add} from "@mui/icons-material";
+import AddTestimonyDialog from "./AddTestimonyDialog";
 
 interface TestimonySideListProps {
     testimonies: Testimony[];
@@ -10,24 +12,41 @@ interface TestimonySideListProps {
 
 const TestimonySideList: React.FC<TestimonySideListProps> = ({testimonies, setTestimonies}) => {
 
+    const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+
     return (
-        <Grid item xs={3}>
-            <MenuList>
-                {testimonies.map((testimony) => (
-                        <MenuItem key={testimony.name}>
-                            <ListItemText>{testimony.name}</ListItemText>
-                            <Typography variant="body2" color="text.secondary">
-                                {testimony.semester}
-                            </Typography>
-                        </MenuItem>
-                ))}
-                {testimonies.length < 4 && (
-                    <MenuItem>
-                        <ListItemText>Add</ListItemText>
-                    </MenuItem>
-                )}
-            </MenuList>
-        </Grid>
+        <>
+            <Grid item xs={3}>
+                <Card elevation={1}>
+                    <CardContent style={{padding: 0}}>
+                        <MenuList>
+                            {testimonies.map((testimony) => (
+                                <MenuItem key={testimony.name}>
+                                    <ListItemText>{testimony.name}</ListItemText>
+                                    <Typography variant="body2" color="text.secondary">
+                                        {testimony.semester}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                            {testimonies.length < 4 && (
+                                <MenuItem onClick={() => setDialogOpen(true)}>
+                                    <ListItemIcon>
+                                        <Add />
+                                    </ListItemIcon>
+                                    <ListItemText>Add</ListItemText>
+                                </MenuItem>
+                            )}
+                        </MenuList>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <AddTestimonyDialog
+                testimonies={testimonies}
+                setTestimonies={setTestimonies}
+                open={dialogOpen}
+                onClose={() => setDialogOpen(false)}
+            />
+        </>
     );
 }
 
